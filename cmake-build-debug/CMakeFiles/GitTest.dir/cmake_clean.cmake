2409a7736968f2a795660ae87732ca5dd6f9d264
Preston-Gibson <95097404+Preston-Gibson@users.noreply.github.com>
2021-11-26 12:33:31 -0700

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/GitTest.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GitTest.dir/main.cpp.obj"
+  "GitTest.exe"
+  "GitTest.exe.manifest"
+  "GitTest.pdb"
+  "libGitTest.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GitTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
